58fa1bcd38e10a0c0ff5a1452638fb18dd8c7672
Hannon22 <71154698+Hannon22@users.noreply.github.com>
2021-09-08 14:30:50 -0700

    bounce

--- hello_p5/hello_p5.js
+++ hello_p5/hello_p5.js
@@ -3,13 +3,20 @@ function setup() {
 }
 
 let x = 50;
+let y = 200;
+let vx = 1;
+let vy = 1;
 
 function draw() {
   background(0);
   fill(255, 0, 0)
-  ellipse(x, 200, 100, 50);
-  x++;
+  ellipse(x, y, 100, 50);
+  x += vx;
+  y += vy
 
-  if (x > width+50)
-    x = -50;
+  if (x > width+50 || x < 50)
+    vx *= -1;
+
+  if (y > height-25 || y < 25)
+    vy *= -1;
 }
