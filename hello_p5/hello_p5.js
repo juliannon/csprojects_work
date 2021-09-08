8feea49addfbc8c354ae085cba897f97ffee0935
Hannon22 <71154698+Hannon22@users.noreply.github.com>
2021-09-08 14:39:26 -0700

    update

--- hello_p5/hello_p5.js
+++ hello_p5/hello_p5.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(400, 400);
 }
 
-let x = 50;
+let x = 25;
 let y = 200;
 let vx = 1;
 let vy = 1;
@@ -10,13 +10,13 @@ let vy = 1;
 function draw() {
   background(0);
   fill(255, 0, 0)
-  ellipse(x, y, 100, 100);
+  ellipse(x, y, 50, 50);
   x += vx;
   y += vy;
 
-  if (x > width+50 || x < 50)
+  if (x > width-25 || x < 25)
     vx *= -1;
 
-  if (y > height-50 || y < 50)
+  if (y > height-25 || y < 25)
     vy *= -1;
 }
